a1b355bacc55b9141948f7fc7f43e99e630af6ec
Duncan Schoenmakers <400156@student.fontys.nl>
2022-01-18 14:58:47 +0100

    added structs and events for webpage

--- contracts/Certificate.sol
+++ contracts/Certificate.sol
@@ -1,24 +1,33 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
+pragma experimental ABIEncoderV2;
 
 contract Certificate
 {
 
-    mapping(string => string[]) private hashes;
+    mapping(string => Certificatestruct[]) private hashes;
 
-    function StoreCertificate(string memory hash, string memory user) public {
-        require
-        hashes[user].push(hash);
+    struct Certificatestruct {
+        string hash;
+        string ipfshash;
+    }
+
+    event storedCertificate(bool done);
+
+    function StoreCertificate(string memory hash, string memory ipfshash, string memory user) public {
+        Certificatestruct memory tempstruct = Certificatestruct(hash, ipfshash);
+        hashes[user].push(tempstruct);
+        emit storedCertificate(true);
     }
     
     function verifyCertificateFromUser(string memory hashFromCertificate, string memory user) public view returns (bool)
     {
         bool isActualDocument = false;
-        string[] memory hashesFromUser = hashes[user];
+        Certificatestruct[] memory hashesFromUser = hashes[user];
         for(uint i=0; i<hashesFromUser.length; i++)
         {
-            if(keccak256(bytes(hashesFromUser[i])) == keccak256(bytes(hashFromCertificate)))
+            if(keccak256(bytes(hashesFromUser[i].hash)) == keccak256(bytes(hashFromCertificate)))
             {
                 isActualDocument = true;
             }
@@ -26,8 +35,13 @@ contract Certificate
         return isActualDocument;
     }
 
-    function findCertificates(string memory user) public view returns (string[] memory)
+    function findCertificates(string memory user) public view returns (Certificatestruct[] memory)
     {
         return hashes[user];
+    }
+
+    function getBalance() public view returns(uint)
+    {
+    return address(this).balance;
     }
 }
